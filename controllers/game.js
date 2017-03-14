4ea90b9bcf8c5155469ca94729229e510d3279d6
Tim3Ds <Tim3Ds@gmail.com>
2017-03-14 11:44:29 -0600

    add game pages and js files empty

--- controllers/game.js
+++ controllers/game.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET game page. */
 router.get('/', function(req, res, next) {
+  var noUser = {
+    username: 'No User',
+    gameID: ''
+  };
+
+  if(req.user == null){
+    req.user = noUser;
+  }
   res.render('game', { 
+    userName: req.user.username,
+    gameID: req.user.gameID
+  });
+});
+router.get('/join', function(req, res, next) {
+    var noUser = {
+    username: 'No User',
+    gameID: Math.random().toString(36).substr(2, 4).toUpperCase()
+  };
+
+  if(req.user == null){
+    req.user = noUser;
+  }
+
+  res.render('JoinGame', { 
     userName: req.user.username,
     gameID: req.user.gameID
   });
